94b1da966975550475d913380dc915f9f47d2113
Vivek Goyal <vgoyal@redhat.com>
2018-08-29 11:15:58 -0400

    fuse: Add logic to free up a memory range
    Add logic to free up a busy memory range. Freed memory range will be
    returned to free pool. Add a worker which can be started to select
    and free some busy memory ranges.
    
    In certain cases (write path), process can steal one of its busy
    dax ranges if free range is not available.
    
    If free range is not available and nothing can't be stolen from same
    inode, caller waits on a waitq for free range to become available.
    
    For reclaiming a range, as of now we need to hold following locks in
    specified order.
    
    	inode_trylock(inode);
    	down_write(&fi->i_mmap_sem);
    	down_write(&fi->i_dmap_sem);
    
    This means, one can not wait for a range to become free when in fault
    path because it can lead to deadlock in following two situations.
    
    - Worker thread to free memory might block on fuse_inode->i_mmap_sem as well.
    - This inode is holding all the memory and more memory can't be freed.
    
    In both the cases, deadlock will ensue. So return -ENOSPC from iomap_begin()
    in fault path if memory can't be allocated. Drop fuse_inode->i_mmap_sem,
    and wait for a free range to become available and retry.
    
    read path can't do direct reclaim as well because it holds shared inode
    lock while reclaim assumes that inode lock is held exclusively. Due to
    shared lock, it might happen that one reader is still reading from range
    and another reader reclaims that range leading to problems. So read path
    also returns -ENOSPC and higher layers retry (like fault path).
    
    write path is a different story. We hold inode lock and lock ordering
    allows to grab fuse_inode->immap_sem, if needed. That means we can do direct
    reclaim in that path. But if there is no memory allocated to this inode,
    then direct reclaim will not work and we need to wait for a memory range
    to become free. So try following order.
    
    A. Try to get a free range.
    B. If not, try direct reclaim.
    C. If not, wait for a memory range to become free
    
    Here sleeping with locks held should be fine because in step B, we made
    sure this inode is not holding any ranges. That means other inodes are
    holding ranges and somebody should be able to free memory. Also, worker
    thread does a trylock() on inode lock. That means worker tread will not
    wait on this inode and move onto next memory range. Hence above sequence
    should be deadlock free.
    
    Signed-off-by: Vivek Goyal <vgoyal@redhat.com>
    Signed-off-by: kbuild test robot <lkp@intel.com>

--- fs/fuse/fuse_i.h
+++ fs/fuse/fuse_i.h
@@ -57,6 +57,16 @@
 #define FUSE_DAX_MEM_RANGE_SZ	(2*1024*1024)
 #define FUSE_DAX_MEM_RANGE_PAGES	(FUSE_DAX_MEM_RANGE_SZ/PAGE_SIZE)
 
+/* Number of ranges reclaimer will try to free in one invocation */
+#define FUSE_DAX_RECLAIM_CHUNK		(10)
+
+/*
+ * Dax memory reclaim threshold in percetage of total ranges. When free
+ * number of free ranges drops below this threshold, reclaim can trigger
+ * Default is 20%
+ * */
+#define FUSE_DAX_RECLAIM_THRESHOLD	(20)
+
 /** List of active connections */
 extern struct list_head fuse_conn_list;
 
@@ -108,6 +118,9 @@ struct fuse_forget_link {
 
 /** Translation information for file offsets to DAX window offsets */
 struct fuse_dax_mapping {
+	/* Pointer to inode where this memory range is mapped */
+	struct inode *inode;
+
 	/* Will connect in fc->free_ranges to keep track of free memory */
 	struct list_head list;
 
@@ -891,12 +904,20 @@ struct fuse_conn {
 	unsigned long nr_busy_ranges;
 	struct list_head busy_ranges;
 
+	/* Worker to free up memory ranges */
+	struct delayed_work dax_free_work;
+
+	/* Wait queue for a dax range to become free */
+	wait_queue_head_t dax_range_waitq;
+
 	/*
 	 * DAX Window Free Ranges. TODO: This might not be best place to store
 	 * this free list
 	 */
 	long nr_free_ranges;
 	struct list_head free_ranges;
+
+	unsigned long nr_ranges;
 };
 
 static inline struct fuse_conn *get_fuse_conn_super(struct super_block *sb)
@@ -1275,6 +1296,7 @@ unsigned fuse_len_args(unsigned numargs, struct fuse_arg *args);
  */
 u64 fuse_get_unique(struct fuse_iqueue *fiq);
 void fuse_free_conn(struct fuse_conn *fc);
+void fuse_dax_free_mem_worker(struct work_struct *work);
 void fuse_cleanup_inode_mappings(struct inode *inode);
 
 #endif /* _FS_FUSE_I_H */
