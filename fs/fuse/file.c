5986af48e817d7a778ab04955a4ee342d7762e46
Vivek Goyal <vgoyal@redhat.com>
2018-11-12 13:11:10 -0500

    fuse: Clear setuid bit even in cache=never path
    If fuse daemon is started with cache=never, fuse falls back to direct IO.
    In that write path we don't call file_remove_privs() and that means setuid
    bit is not cleared if unpriviliged user writes to a file with setuid bit set.
    
    pjdfstest chmod test 12.t tests this and fails.
    
    Fix this by calling fuse_remove_privs() even for direct I/O path.
    
    I tested this as follows.
    
    - Run fuse example pasthrough fs.
    
      $ passthrough_ll /mnt/pasthrough-mnt -o default_permissions,allow_other,cache=never
      $ mkdir /mnt/pasthrough-mnt/testdir
      $ cd /mnt/pasthrough-mnt/testdir
      $ prove -rv pjdfstests/tests/chmod/12.t
    
    Signed-off-by: Vivek Goyal <vgoyal@redhat.com>

--- fs/fuse/file.c
+++ fs/fuse/file.c
@@ -1465,14 +1465,18 @@ static ssize_t fuse_direct_write_iter(struct kiocb *iocb, struct iov_iter *from)
 	/* Don't allow parallel writes to the same file */
 	inode_lock(inode);
 	res = generic_write_checks(iocb, from);
-	if (res > 0) {
-		if (!is_sync_kiocb(iocb) && iocb->ki_flags & IOCB_DIRECT) {
-			res = fuse_direct_IO(iocb, from);
-		} else {
-			res = fuse_direct_io(&io, from, &iocb->ki_pos,
-					     FUSE_DIO_WRITE);
-		}
+	if (res <= 0)
+		goto out;
+
+	res = file_remove_privs(iocb->ki_filp);
+	if (res)
+		goto out;
+	if (!is_sync_kiocb(iocb) && iocb->ki_flags & IOCB_DIRECT) {
+		res = fuse_direct_IO(iocb, from);
+	} else {
+		res = fuse_direct_io(&io, from, &iocb->ki_pos, FUSE_DIO_WRITE);
 	}
+out:
 	fuse_invalidate_attr(inode);
 	if (res > 0)
 		fuse_write_update_size(inode, iocb->ki_pos);
